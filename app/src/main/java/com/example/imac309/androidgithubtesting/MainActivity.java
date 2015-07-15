5067f95c0a244c118b3e8acb554624a39277f844
vishalMobiledeveloper <vishaliphonedeveloper@gmail.com>
2015-07-15 18:31:37 +0530

    Add Toast Messsage

--- app/src/main/java/com/example/imac309/androidgithubtesting/MainActivity.java
+++ app/src/main/java/com/example/imac309/androidgithubtesting/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(getApplicationContext(),"Called",Toast.LENGTH_LONG).show();
     }
 
     @Override
